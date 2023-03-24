464a5d1c049771c4a5b6dfe88ccb36ebd89100a4
davischan <davis.chan@aol.com>
2023-03-24 17:57:27 +0800

    fix(decorator): should always return an array for messages

--- src/decorators/formatted-messages.decorator.ts
+++ src/decorators/formatted-messages.decorator.ts
@@ -1,15 +1,15 @@
-import { HttpException, SetMetadata } from '@nestjs/common';
+import { SetMetadata } from '@nestjs/common';
 import { FORMATTED_MESSAGE_METADATA } from '../constants';
 
-export function FormattedMessages(messages: string | string[], exception?: HttpException) {
+export function FormattedMessages(messages: string | string[], exception?: Error) {
+    if (typeof messages === 'string') {
+        messages = [messages];
+    }
     if (exception) {
         const newException = Object.create(exception);
         Reflect.defineMetadata(FORMATTED_MESSAGE_METADATA, messages, newException.constructor);
         return newException;
     } else {
-        if (typeof messages === 'string') {
-            messages = [messages];
-        }
         return SetMetadata(FORMATTED_MESSAGE_METADATA, messages);
     }
 }
